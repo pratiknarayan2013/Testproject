a9d99e59b531ce72c669a79bca445ac34ff597f9
Pratik <Pratik@Pratik-Lenovo>
2022-03-04 09:23:02 +0530

    commit

---
+++ Java8_Features/src/LambdaExp/Example1.java
@@ -0,0 +1,20 @@
+package LambdaExp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		
+		List<String> list = new ArrayList<String>();
+		list.add("Cricket");
+		list.add("Hockey");
+		list.add("Chess");
+		System.out.println("Via lambda Expressions");
+		list.forEach(game->System.out.println(game));
+		System.out.println("Via Method Reference");
+		list.forEach(System.out::println);
+	}
+	
+}
